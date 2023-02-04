67d047ae4581ccd8f466f594fb3b2cdee12d29ea
AJ <burlywalter@gmail.com>
2023-02-04 13:00:58 -0500

    I added text to Fav Screen

--- Freemind/VViews/FavoritesView.swift
+++ Freemind/VViews/FavoritesView.swift
@@ -24,6 +24,7 @@ struct FavoritesView: View {
                     ScrollView {
                         VStack {
                           
+                            Text("HI")
                             ForEach(workSpaceFavorite.favoriteWorkSpace)  { workSpace in
                                 SpaceRoomLayout(workSpace: workSpace, workSpaceFavorite: workSpaceFavorite)
                                 
