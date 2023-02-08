963f1282036fd2d4e1f8f45a98196c2df2d613c0
Artavis Sanders <iamartavis@gmail.com>
2023-02-08 11:15:31 -0500

    Added charts into this file.

---
+++ Freemind/VViews/RealTimeDataView.swift
@@ -0,0 +1,34 @@
+//
+//  RealTimeDataView.swift
+//  Freemind
+//
+//  Created by Artavis Sanders on 2/7/23.
+//
+
+import SwiftUI
+import Charts
+
+
+
+struct DataView: View {
+    let ambience = ["crowd":0, "cleanliness":5, "noise":1, "temperature":2]
+    var body: some View {
+        Chart(ambience.keys.sorted(), id: \.self) {
+            BarMark(
+                x: .value("person", $0),
+                y: .value("cups bought", ambience[$0] ?? 0)
+            )
+        }.frame(width: 350, height: 300)
+            .chartYAxisLabel("Quality",alignment:.center)
+            .chartYAxis {
+                AxisMarks(position: .leading)
+            }
+            .chartYScale(domain: 0...20)
+    }
+}
+
+struct Contentiew: PreviewProvider {
+    static var previews: some View {
+       DataView()
+    }
+}
